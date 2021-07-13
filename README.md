09d9629b064ea9790663dad00ddde74b3d7cc768
Jørgen Halvorsen <jorgen.aunet.halvorsen@gmail.com>
2021-07-13 18:25:07 +0200

    feat: add user admin script

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 [![Build and deploy](https://github.com/halv00rsen/vanligfyr/actions/workflows/firebase-hosting-merge.yml/badge.svg?branch=main)](https://github.com/halv00rsen/vanligfyr/actions/workflows/firebase-hosting-merge.yml)
 
 Today I learned app
+
+For interacting with the admin user scripts:
+
+```
+export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/service-account-file.json"
+```
+
+For more information, take a look at the [documentation](https://firebase.google.com/docs/admin/setup).
---
+++ admin/src/index.ts
@@ -0,0 +1,60 @@
+import admin from 'firebase-admin';
+import { createInterface } from 'readline';
+
+const databaseURL =
+  'https://vanligfyr-default-rtdb.europe-west1.firebasedatabase.app';
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL,
+});
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('Email? ', async (email) => {
+  console.log(`Finding user ${email}`);
+  if (email) {
+    try {
+      const userRecord = await admin.auth().getUserByEmail(email);
+      const claims = userRecord.customClaims || { roles: [] };
+      const roles = claims.roles as string[];
+      const isAdmin = roles.includes('admin');
+      if (isAdmin) {
+        console.log(`The user is admin`);
+      } else {
+        console.log('The user is NOT admin');
+      }
+      readline.question('Make admin? (y/n/C) ', (input) => {
+        if (input === 'y') {
+          const newRoles = isAdmin
+            ? [...roles]
+            : [...roles, 'admin'];
+          admin.auth().setCustomUserClaims(userRecord.uid, {
+            ...claims,
+            roles: newRoles,
+          });
+          console.log('user is made admin');
+        } else if (input === 'n') {
+          admin.auth().setCustomUserClaims(userRecord.uid, {
+            ...claims,
+            roles: roles.filter((role) => role !== 'admin'),
+          });
+          console.log('admin privileges is removed');
+        } else {
+          console.log('No change made to user.');
+        }
+        readline.close();
+      });
+    } catch {
+      console.log('An error happend.');
+      console.log(`Could not find user "${email}"`);
+      readline.close();
+    }
+  } else {
+    console.log('Please input a non empty email.');
+    readline.close();
+  }
+});
