20cbfa342afabfea0f8f2ef7daaaf75a0c153249
Jørgen Halvorsen <jorgen.aunet.halvorsen@gmail.com>
2022-04-30 11:54:56 +0200

    feat: use replace prizm with hjls for bundling

--- frontend/src/components/Editor/Markdown.tsx
+++ frontend/src/components/Editor/Markdown.tsx
@@ -1,17 +1,21 @@
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import ReactMarkdown, { Components } from 'react-markdown';
+import styles from './Markdown.module.css';
 
 const components: Components = {
   code({ inline, className, children, ...props }) {
     const match = /language-(\w+)/.exec(className || '');
+    const stringChildren = String(children).replace(/\n$/, '');
+
     return !inline && match ? (
       <SyntaxHighlighter
-        style={vscDarkPlus}
+        style={atomOneDark}
         language={match[1]}
         PreTag="div"
+        codeTagProps={{ className: styles.code }}
       >
-        {String(children).replace(/\n$/, '')}
+        {stringChildren}
       </SyntaxHighlighter>
     ) : (
       <code className={className} {...props}>
