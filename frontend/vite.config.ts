34db9a467e9733b704a9ef4107944869885726fd
Jørgen Halvorsen <jorgen.aunet.halvorsen@gmail.com>
2022-07-11 14:27:57 +0200

    chore: add vite plugin checker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [react(), visualizer(), checker({ typescript: true })],
   server: {
     proxy: {
       // Local proxy to function "user"
