d942e66e4995c67bd1340af9244aaa11bbdf1b9e
Jørgen Halvorsen <jorgen.aunet.halvorsen@gmail.com>
2022-07-11 14:46:28 +0200

    chore: remove check of ts in build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,11 @@ import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer(), checker({ typescript: true })],
+  plugins: [
+    react(),
+    visualizer(),
+    checker({ typescript: true, enableBuild: false }),
+  ],
   server: {
     proxy: {
       // Local proxy to function "user"
